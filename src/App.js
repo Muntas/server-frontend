a29e7fe0cbd50a7b82e208df7cca72c5a9d452e4
Muntas <mantas@krisciunas.net>
2018-09-12 23:06:56 +0100

    fetch1
    fetch1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      placeholder: true,
+      content: undefined
+    };
   }
+
+  componentDidMount() {
+    fetch("http://ec2-35-178-211-121.eu-west-2.compute.amazonaws.com/api").then(
+      response => this.setState({ placeholder: false, content: response })
+    );
+  }
+
   render() {
     return <h1>Hello</h1>;
   }
