cdb0f7e99e57c1204106806bd8d6b8d542a8a917
Muntas <mantas@krisciunas.net>
2018-09-12 23:21:01 +0100

    fetch3
    fetch3

--- src/App.js
+++ src/App.js
@@ -13,11 +13,19 @@ class App extends React.Component {
   componentDidMount() {
     fetch("http://ec2-35-178-211-121.eu-west-2.compute.amazonaws.com/api")
       .then(response => response.json())
-      .then(data => this.setState({ placeholder: false, content: data }));
+      .then(data => this.setState({ placeholder: false, content: data[0] }));
   }
 
   render() {
-    return <h1>Hello</h1>;
+    return (
+      <div>
+        {this.state.placeholder ? (
+          <h1>Hello</h1>
+        ) : (
+          <h1>{this.state.content.title}</h1>
+        )}
+      </div>
+    );
   }
 }
 
